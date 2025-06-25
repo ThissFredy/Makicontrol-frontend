6d1cca2ebfd96db8b55599984a9672be89e8772e
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2025-06-25 13:06:59 -0500

    📕 Initial Commit

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>Please enter your credentials to log in.</p>
+      <form action="/api/login" method="POST">
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default page;
