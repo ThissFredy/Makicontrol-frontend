65e881aa1ab8d6068eb9946490661e5bfbc0d655
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2025-06-25 13:12:40 -0500

    🔩 Deploy error fixed

---
+++ src/app/(main)/customers/contracts/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Customer Contracts</h1>
+      <p>Manage your customer contracts here.</p>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/(main)/customers/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Customer Management</h1>
+      <p>Manage your customers here.</p>
+    </div>
+  );
+};
+
+export default page;
