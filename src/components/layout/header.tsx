f06398f32794ea742f3df0d77de220f5e6cad67e
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2025-07-03 23:43:31 -0500

    🖍️ Styles of header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,19 +1,22 @@
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
 
+// --- Componente Header ---
 const Header = () => {
     return (
-        <header className="bg-[#8C9EC2] shadow-md min-h-[60px] p-2">
-            <div className="container w-full m-4">
-                <Link href="/">
-                    <Image
-                        src="/logo.svg"
-                        alt="Logo de MAKI"
-                        width={40}
-                        height={40}
-                        className="h-10 md:h-15"
-                    />
-                </Link>
+        <header className="bg-[#8C9EC2] shadow-md min-h-[60px]">
+            <div className="w-full">
+                <div className="flex justify-center md:justify-start m-1 items-center h-23">
+                    <Link href="/">
+                        <Image
+                            src="/logo.svg"
+                            alt="Logo de MAKI"
+                            width={150}
+                            height={0}
+                            className="h-10 md:h-12"
+                        />
+                    </Link>
+                </div>
             </div>
         </header>
     );
